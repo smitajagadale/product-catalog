f0afa92d1b3b023f9ef46ccaaa759efc5c1c60c7
smitajagadale <61117979+smitajagadale@users.noreply.github.com>
2020-02-17 20:22:00 +0530

    Add files via upload

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import { getLocalData } from '../actions/crud';
 import ProdTable from './table';
 import "./table.css";
 import { Link } from 'react-router-dom';
+import TableHeader from './tableheader';
 
 
 class Home extends Component {
@@ -24,10 +25,28 @@ class Home extends Component {
         })
     }
 
+    filterSearch = (search) => {
+        search = search && search.length ? search.toLowerCase() : "";
+        const updatedData = this.state.storedData.filter(data => {
+            const productValues = Object.values(data);
+            for (const prodVal of productValues) {
+                const indexOf = prodVal.toLowerCase().indexOf(search);
+                if (indexOf !== -1) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        this.setState({
+            storedData : updatedData,
+        })
+    }
+
     render() {
         const { storedData } = this.state;
         return (
             <div>
+                <TableHeader handleSearch={this.filterSearch} />
                 <ProdTable data={storedData} history={this.props.history} refreshData={this.fetchTableData} />
                 <Link to="/create"><button onClicktype="button" class="btn btn-primary">ADD</button>
                 </Link>
---
+++ src/components/tableheader.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class TableHeader extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            search: ""
+        }
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleOnSearch = (e) => {
+        e.preventDefault();
+        this.props.handleSearch(this.state.search);
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-dark bg-primary justify-content-between">
+                    <a className="text-warning h3">Product Catalogue</a>
+                    <form className="form-inline">
+                        <input className="form-control mr-sm-2" type="search" id="search" onChange={this.handleOnChange} placeholder="Search" />
+                        <button className="btn btn-success" onClick={this.handleOnSearch} type="submit">Search</button>
+                    </form>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default TableHeader;
